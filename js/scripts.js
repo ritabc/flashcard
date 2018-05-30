3eb48ae1c2f7d702672fe220a052c9f3f741d73a
Rita Bennett-Chew and Will Swanson <will@>
2018-05-30 10:16:12 -0700

    updated html with data for flashcards, updated css and javascript with show hide functionality for flash cards.

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  $("#JS p").click(function(){
+    $("#JS p.definition").toggle();
+    $("#JS p.term").toggle();
+  });
+  $("#operators p").click(function(){
+    $("#operators p.definition").toggle();
+    $("#operators p.term").toggle();
+  });
+  $("#variable p").click(function(){
+    $("#variable p.definition").toggle();
+    $("#variable p.term").toggle();
+  });
+  $("#variablenc p").click(function(){
+    $("#variablenc p.definition").toggle();
+    $("#variablenc p.term").toggle();
+  });
+  $("#functions p").click(function(){
+    $("#functions p.definition").toggle();
+    $("#functions p.term").toggle();
+  });
+  $("#methods p").click(function(){
+    $("#methods p.definition").toggle();
+    $("#methods p.term").toggle();
+  });
+  $("#arguments p").click(function(){
+    $("#arguments p.definition").toggle();
+    $("#arguments p.term").toggle();
+  });
+  $("#return p").click(function(){
+    $("#return p.definition").toggle();
+    $("#return p.term").toggle();
+  });
+  $("#chainingMethods p").click(function(){
+    $("#chainingMethods p.definition").toggle();
+    $("#chainingMethods p.term").toggle();
+  });
+  $("#strings p").click(function(){
+    $("#strings p.definition").toggle();
+    $("#strings p.term").toggle();
+  });
+  $("#booleans p").click(function(){
+    $("#booleans p.definition").toggle();
+    $("#booleans p.term").toggle();
+  });
+  $("#undefined p").click(function(){
+    $("#undefined p.definition").toggle();
+    $("#undefined p.term").toggle();
+  });
+  $("#undefined p").click(function(){
+    $("#undefined p.definition").toggle();
+    $("#undefined p.term").toggle();
+});
